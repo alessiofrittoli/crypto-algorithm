7ab99bc0d34595783aa50481851ae4a01a6d0ec7
Alessio Frittoli <info@alessiofrittoli.it>
2025-05-03 13:55:34 +0200

    chore: minor improvements

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,20 @@
 import type { JestConfigWithTsJest } from 'ts-jest'
 import dotenv from 'dotenv'
 
+console.log(
+`
+___________________________________________________________
+        ___    __                 _ __  __            
+       /   |  / /___ _____  _____(_) /_/ /_  ____ ___ 
+      / /| | / / __ \\\`/ __ \\/ ___/ / __/ __ \\/ __ \\\`__ \\
+     / ___ |/ / /_/ / /_/ / /  / / /_/ / / / / / / / /
+    /_/  |_/_/\\__, /\\____/_/  /_/\\__/_/ /_/_/ /_/ /_/ 
+             /____/                                   
+___________________________________________________________
+`
+)
+
+
 const env = process.env.NODE_ENV
 
 dotenv.config( { path: [
@@ -19,7 +33,7 @@ const config: JestConfigWithTsJest = {
 	/** https://jestjs.io/docs/configuration#testenvironment-string */
 	testEnvironment: 'node',
 	moduleDirectories: [ 'node_modules', '<rootDir>/' ],
-	testMatch: [ '**/__tests__/**/*.test.ts' ],
+	testMatch: [ '**/__tests__/**/*.(test|spec).ts' ],
 	/**
 	 * If you're using [Module Path Aliases](https://nextjs.org/docs/advanced-features/module-path-aliases),
 	 * you will have to add the moduleNameMapper in order for jest to resolve your absolute paths.
